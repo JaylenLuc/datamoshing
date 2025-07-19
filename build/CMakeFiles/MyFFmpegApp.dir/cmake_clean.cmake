05e7511c6239d1b7e2c18370abd167931d6c98d9
jaylenluc <jaylenluc1@gmail.com>
2025-07-19 11:25:21 -0700

    eveyrthing

---
+++ build/CMakeFiles/MyFFmpegApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyFFmpegApp.dir/main.cpp.o"
+  "CMakeFiles/MyFFmpegApp.dir/main.cpp.o.d"
+  "MyFFmpegApp"
+  "MyFFmpegApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyFFmpegApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MyFFmpegApp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MyFFmpegApp.
+# This may be replaced when dependencies are built.
