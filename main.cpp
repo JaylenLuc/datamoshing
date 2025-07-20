a5d743c4559b8351a2f4edb9c7940662e8822fc8
jaylenluc <jaylenluc1@gmail.com>
2025-07-19 19:08:52 -0700

    save this shit

--- main.cpp
+++ main.cpp
@@ -7,11 +7,31 @@ extern "C" {
     #include <libavutil/avutil.h>
     #include <libswscale/swscale.h>
 }
+
 int main(int argc, char* argv[]) {
+    const char* mosh_type;
+
     if (argc < 2) {
         std::cout << "Usage: " << argv[0] << " <video_file>" << std::endl;
         return 1;
     }
+    if (std::string(argv[1]) == "-h") {
+        std::cout << "All available options: " << std::endl;
+        std::cout << "-skipIMod" << std::endl;
+        return 1;
+    }
+    if (argc  == 2){
+        return 1;
+    }
+    if (std::string(argv[2]) == "-skipIMod") {
+        mosh_type = "-skipIMod";
+    }else{
+        mosh_type = "-skipIMod";
+    }
+    for (int i = 0; i < argc; ++i) {
+        std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+    }
+
     const char* input_file = argv[1];
     // open the input file and allocate the format context
     AVFormatContext* formatContext = nullptr;
@@ -24,6 +44,9 @@ int main(int argc, char* argv[]) {
         std::cout << "Could not find stream information" << std::endl;
         return 3;
     }
+    AVFormatContext* outFormatCtx = nullptr;
+    avformat_alloc_output_context2(&outFormatCtx, nullptr, nullptr, "data_moshed.mp4");
+    std::cout << "Creating output context for: data_moshed.mp4" << std::endl;
     std::cout << "Finding stream info: " << input_file << std::endl;
     //here we find the video stream
     int video_stream_idx = -1;
@@ -53,8 +76,10 @@ int main(int argc, char* argv[]) {
     AVFrame* frame = av_frame_alloc();
     //frame gets encoeded into a packet 
     AVPacket* packet = av_packet_alloc();
+    unsigned int frame_count = 0;
     while (av_read_frame(formatContext, packet) == 0) {
         std::cout << " reading frame" << std::endl;
+        frame_count++;
         if (packet->stream_index == video_stream_idx) {
             //decoding
             int ret = avcodec_send_packet(codec_context, packet);
@@ -65,17 +90,27 @@ int main(int argc, char* argv[]) {
             while (ret >= 0 ) {
                 ret = avcodec_receive_frame(codec_context, frame);
                 if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
-                    break; // No more frames to decode
+                    break; 
                 } else if (ret < 0) {
                     std::cout << "Error receiving frame: " << std::endl;
                     break;
                 }
                 //if still frames left and no error then we can process the frame
+                char frame_data_type = av_get_picture_type_char(frame->pict_type);
+                std::cout << "*************************" << std::endl;
                 std::cout << "Frame " << codec_context->frame_num
-                          << "; type=" << av_get_picture_type_char(frame->pict_type)
+                          << "; type=" << frame_data_type
                           << "; bytes pts " << frame->pts
                           << "; format" << frame->pict_type
                           << std::endl;
+                std::cout << "*************************" << std::endl;
+                if (frame_data_type == AV_PICTURE_TYPE_I) {
+
+                    continue;
+                }
+                else if (frame_data_type == AV_PICTURE_TYPE_P) {
+                    // Process P-frames
+                } 
                 
             }
         }
